7aec7fa35ddae0c5bb5f89a78db236738842c17c
olegacaten <116825087+olegacaten@users.noreply.github.com>
2023-09-28 06:18:33 -0700

    all3

--- app/src/routes/Routing.tsx
+++ app/src/routes/Routing.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import Layout from '../layouts/Layout'
+import Projects from '../pages/projects/Projects'
+import Layout from '../layouts/Layout';
 function Routing() {
-    const isAuth = true;
     return (
       <BrowserRouter>
         <Routes>
           <Route path={""} element={<Layout />}>
-              <Route index element={<div>layout</div>}/>
+              <Route index element={<Projects/>}/>
               <Route path={'/todos'} element={<div>todos</div>}/>
               <Route path='*' element={<div>404</div>}/>          
           </Route>   
